dd844706506dcfd82d802d8012ecccbeaadf7c28
Andrew Ni <niandrew7@gmail.com>
2021-10-21 16:25:47 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cap-firstboot.sh
+++ cap-firstboot.sh
@@ -1,3 +1,6 @@
+wget $(ovfenv -k cse.configUrl) -O cse-config.yaml
+chmod 600 cse-config.yaml
+
 cse install -c config.yaml -s
 
 systemctl enable cse
--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,7 @@
 yum update -y
 yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python
 pip3 install --upgrade pip
-git clone https://github.com/vmware/container-service-extension.git
+git clone $(ovfenv -k cse.repositoryUrl)
+cd container-service-extension
+git checkout $(ovfenv -k cse.branch)
 pip3 install --ignore-installed container-service-extension/

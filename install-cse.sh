24554372f22022ca74992871916ef9cd538b0fe4
Andrew Ni <niandrew7@gmail.com>
2021-10-21 16:01:36 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- install-cse.sh
+++
@@ -1,5 +0,0 @@
-yum update -y
-yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers
-pip3 install --upgrade pip
-git clone https://github.com/vmware/container-service-extension.git
-pip3 install container-service-extension/

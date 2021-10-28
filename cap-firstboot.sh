c7251115e796e5f928a132bc11ff1db31db8217e
Andrew Ni <niandrew7@gmail.com>
2021-10-28 01:34:03 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cap-firstboot.sh
+++ cap-firstboot.sh
@@ -1,3 +1,13 @@
+yum update -y
+yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python
+pip3 install --upgrade pip
+git clone https://github.com/vmware/container-service-extension
+pip3 install --ignore-installed container-service-extension/
+
+git clone https://github.com/andrew-ni/cse-ovf.git
+mv cse-ovf/cse.service cse-ovf/systemd/system/cse.service
+mv cse-ovf/cse-run.sh /root/cse-run.sh
+
 wget $(ovfenv -k cse.configUrl) -O cse-config.yaml
 chmod 600 cse-config.yaml
 
--- post-install.sh
+++
@@ -1,9 +0,0 @@
-yum update -y
-yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python
-pip3 install --upgrade pip
-git clone https://github.com/vmware/container-service-extension
-pip3 install --ignore-installed container-service-extension/
-
-git clone https://github.com/andrew-ni/cse-ovf.git
-mv cse-ovf/cse.service cse-ovf/systemd/system/cse.service
-mv cse-ovf/cse-run.sh /root/cse-run.sh

3f9c42fa3e0b3417f301b8e302bc482ced9d1ee7
Andrew Ni <niandrew7@gmail.com>
2021-10-29 10:01:03 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cap-firstboot.sh
+++ cap-firstboot.sh
@@ -7,13 +7,15 @@ pip3 install --ignore-installed container-service-extension/
 git clone https://github.com/andrew-ni/cse-ovf.git
 mv cse-ovf/cse.service /etc/systemd/system/cse.service
 mv cse-ovf/cse-run.sh /root/cse-run.sh
-mv cse-ovf/cse-init.sh /var/lib/cloud/scripts/per-instance/cse-init.sh
+mv cse-ovf/cse-init-per-boot.yaml /var/lib/cloud/scripts/per-boot/cse-init-per-boot.yaml
+mv cse-ovf/cse-init-per-instance.yaml /var/lib/cloud/scripts/per-instance/cse-init-per-instance.yaml
 
-echo $(ovfenv -k cse.configUrl) > /root/sanity-cap-firstboot.yaml
-wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
-chmod 600 /root/cse-config.yaml
 
-cse install -c /root/cse-config.yaml -s -t
+# echo $(ovfenv -k cse.configUrl) > /root/sanity-cap-firstboot.yaml
+# wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
+# chmod 600 /root/cse-config.yaml
 
-systemctl enable cse
-systemctl start cse
+# cse install -c /root/cse-config.yaml -s -t
+
+# systemctl enable cse
+# systemctl start cse
--- cse-init.sh
+++
@@ -1,8 +0,0 @@
-echo $(ovfenv -k cse.configUrl) > /root/sanity-cse-init.yaml
-wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
-chmod 600 /root/cse-config.yaml
-
-cse install -c /root/cse-config.yaml -s -t
-
-systemctl enable cse
-systemctl start cse

44631d7e814154d0d22142e49f8abe1b0f1c79d4
Andrew Ni <niandrew7@gmail.com>
2021-10-29 12:52:39 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cap-firstboot.sh
+++ cap-firstboot.sh
@@ -3,3 +3,5 @@ yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binuti
 pip3 install --upgrade pip
 git clone https://github.com/vmware/container-service-extension
 pip3 install --ignore-installed container-service-extension/
+chmod u+x /var/lib/cloud/scripts/per-boot/cse-init-per-boot.sh
+chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh

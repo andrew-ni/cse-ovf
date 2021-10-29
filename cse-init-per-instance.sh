8be153394a82b2ecff23b3b01af21b3c8712f581
Andrew Ni <niandrew7@gmail.com>
2021-10-29 12:59:36 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cse-init-per-instance.sh
+++ cse-init-per-instance.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+touch /root/cse-init-per-boot
 echo $(ovfenv -k cse.configUrl) >> /root/cse-init-per-boot
-echo hello >> /root/cse-init-per-boot
-wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
-chmod 600 /root/cse-config.yaml
-cse install -c /root/cse-config.yaml -s -t
-systemctl enable cse
-systemctl start cse
+# echo hello >> /root/cse-init-per-boot
+# wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
+# chmod 600 /root/cse-config.yaml
+# cse install -c /root/cse-config.yaml -s -t
+# systemctl enable cse
+# systemctl start cse

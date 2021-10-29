75ea991b65f2d432b06d2b749be291bd4ba19d03
Andrew Ni <niandrew7@gmail.com>
2021-10-29 11:52:28 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cap-firstboot.sh
+++ cap-firstboot.sh
@@ -3,19 +3,3 @@ yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binuti
 pip3 install --upgrade pip
 git clone https://github.com/vmware/container-service-extension
 pip3 install --ignore-installed container-service-extension/
-
-# git clone https://github.com/andrew-ni/cse-ovf.git
-# mv cse-ovf/cse.service /etc/systemd/system/cse.service
-# mv cse-ovf/cse-run.sh /root/cse-run.sh
-# mv cse-ovf/cse-init-per-boot.yaml /var/lib/cloud/scripts/per-boot/cse-init-per-boot.yaml
-# mv cse-ovf/cse-init-per-instance.yaml /var/lib/cloud/scripts/per-instance/cse-init-per-instance.yaml
-
-
-# echo $(ovfenv -k cse.configUrl) > /root/sanity-cap-firstboot.yaml
-# wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
-# chmod 600 /root/cse-config.yaml
-
-# cse install -c /root/cse-config.yaml -s -t
-
-# systemctl enable cse
-# systemctl start cse

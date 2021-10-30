98bbde965f0261dd2f59f279669d47bfae3c3fc6
Andrew Ni <niandrew7@gmail.com>
2021-10-29 17:03:38 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- post-install.sh
+++ post-install.sh
@@ -1,10 +1,9 @@
 echo postinstall >> /root/a
-ls -la /var/lib/cloud/scripts/per-boot/ >> /root/postinstall
+ls -la /var/lib/cloud/scripts/per-boot/ >> /root/a
 
-touch /root/postinstall
-echo $(ovfenv -k cse.configUrl) >> /root/postinstall
+echo $(ovfenv -k cse.configUrl) >> /root/a
 wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
 chmod u+x /var/lib/cloud/scripts/per-boot/cse-init-per-boot.sh
 chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
 
-echo postinstall >> /root/postinstall
+echo postinstall >> /root/a

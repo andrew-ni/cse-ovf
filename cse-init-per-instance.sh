2774f895682757528eeb35db87c2e21f3cb60420
Andrew Ni <niandrew7@gmail.com>
2021-10-30 01:48:07 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cse-init-per-instance.sh
+++ cse-init-per-instance.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-# touch /root/cse-init-per-boot
-# echo $(ovfenv -k cse.configUrl) >> /root/cse-init-per-boot
-# echo hello >> /root/cse-init-per-boot
-# wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
-# chmod 600 /root/cse-config.yaml
-# cse install -c /root/cse-config.yaml -s -t
-# systemctl enable cse
-# systemctl start cse
-
 echo cseinitperinstance >> /root/a
-# ls -la /var/lib/cloud/scripts/per-boot/ >> /root/a
-# echo "ovfenv $(ovfenv)" >> /root/a
-vmtoolsd --cmd "info-get guestinfo.ovfenv" >> /root/cse-init-per-instance-xml
-perl -ne 'print $1,"\n" if (m/cse\.configUrl.*oe:value="(.*?)"/)' /root/cse-init-per-instance >> /root/cse-init-per-instance-url
+vmtoolsd --cmd "info-get guestinfo.ovfenv" > /root/cse-init-per-instance-xml
+perl -ne 'print $1,"\n" if (m/cse\.configUrl.*oe:value="(.*?)"/)' /root/cse-init-per-instance-xml > /root/cse-init-per-instance-url
+wget $(cat /root/cse-init-per-instance-url) -O /root/cse-config.yaml
+chmod 600 /root/cse-config.yaml
+cse install -c /root/cse-config.yaml -s -t > /root/cse-install-output.log
+systemctl enable cse
+systemctl start cse
 echo cseinitperinstance >> /root/a

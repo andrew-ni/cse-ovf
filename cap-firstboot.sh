6ceaae5e1a6ffc8fe60aba7c4faf0535b9b8624d
Andrew Ni <niandrew7@gmail.com>
2021-10-29 17:02:35 -0700

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cap-firstboot.sh
+++ cap-firstboot.sh
@@ -3,9 +3,9 @@ yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binuti
 pip3 install --upgrade pip
 git clone https://github.com/vmware/container-service-extension
 pip3 install --ignore-installed container-service-extension/
-chmod u+x /var/lib/cloud/scripts/per-boot/cse-init-per-boot.sh
-chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
-sed -i 's/scripts-user/[scripts-user, always]/g' /etc/cloud/cloud.cfg
+# chmod u+x /var/lib/cloud/scripts/per-boot/cse-init-per-boot.sh
+# chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
+# sed -i 's/scripts-user/[scripts-user, always]/g' /etc/cloud/cloud.cfg
 
 echo capfirstboot >> /root/a
 ls -la /var/lib/cloud/scripts/per-boot/ >> /root/a
--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,10 @@
 echo postinstall >> /root/a
-ls -la /var/lib/cloud/scripts/per-boot/ >> /root/a
-echo postinstall >> /root/a
+ls -la /var/lib/cloud/scripts/per-boot/ >> /root/postinstall
+
+touch /root/postinstall
+echo $(ovfenv -k cse.configUrl) >> /root/postinstall
+wget $(ovfenv -k cse.configUrl) -O /root/cse-config.yaml
+chmod u+x /var/lib/cloud/scripts/per-boot/cse-init-per-boot.sh
+chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
+
+echo postinstall >> /root/postinstall

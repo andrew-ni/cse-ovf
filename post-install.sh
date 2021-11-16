afdd2686afd9b9fb328b117a9b63dc470d3a8d25
Andrew Ni <niandrew7@gmail.com>
2021-11-16 12:25:08 -0800

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- post-install.sh
+++ post-install.sh
@@ -1,6 +1,6 @@
 chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
 
-# yum update -y >> /root/post-install.log
+yum update -y >> /root/post-install.log
 # yum -y install git >> /root/post-install.log
 # yum -y install --downloadonly git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python >> /root/post-install.log
 # pip3 install --upgrade pip >> /root/post-install.log

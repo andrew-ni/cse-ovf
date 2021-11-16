720f1042448c8b882cc4ad974963640f86b3113c
Andrew Ni <niandrew7@gmail.com>
2021-11-16 09:48:35 -0800

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cse-init-per-instance.sh
+++ cse-init-per-instance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-yum update -y
-yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python
-pip3 install --upgrade pip
-git clone -b cse_3_1_updates https://github.com/vmware/container-service-extension
-pip3 install --ignore-installed container-service-extension/
+# yum update -y
+# yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python
+# pip3 install --upgrade pip
+# git clone -b cse_3_1_updates https://github.com/vmware/container-service-extension
+# pip3 install --ignore-installed container-service-extension/
 
 vmtoolsd --cmd "info-get guestinfo.ovfenv" > /root/ovfenv
 perl -ne 'print $1,"\n" if (m/cse\.configUrl.*oe:value="(.*?)"/)' /root/ovfenv > /root/configUrl

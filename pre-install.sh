e9ee4b100ed6ed3a1bac7e2f6c5d97f34c202823
Andrew Ni <niandrew7@gmail.com>
2021-11-16 10:10:35 -0800

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- pre-install.sh
+++ pre-install.sh
@@ -1,7 +1,7 @@
 chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
 
 yum update -y >> /root/pre-install.log
-yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python >> /root/pre-install.log
+yum -y install --downloadonly git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python >> /root/pre-install.log
 pip3 install --upgrade pip >> /root/pre-install.log
 git clone -b cse_3_1_updates https://github.com/vmware/container-service-extension
 pip3 install --ignore-installed container-service-extension/ >> /root/pre-install.log

4f05846d4b2f3dea64e6d7ddcb03bd5ff0a29066
Andrew Ni <niandrew7@gmail.com>
2021-11-16 10:42:28 -0800

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,9 @@
 chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
 
-yum update -y >> /root/post-install.log
-yum -y install git python3-pip >> /root/post-install.log
-yum -y install --downloadonly gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python >> /root/post-install.log
-pip3 install --upgrade pip >> /root/post-install.log
-git clone -b cse_3_1_updates https://github.com/vmware/container-service-extension
-pip3 install --ignore-installed container-service-extension/ >> /root/post-install.log
-yum -y remote git python3-pip >> /root/post-install.log
+yum update -y >> /root/pre-install.log
+yum -y install git >> /root/pre-install.log
+yum -y install --downloadonly python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python >> /root/pre-install.log
+# pip3 install --upgrade pip >> /root/pre-install.log
+# git clone -b cse_3_1_updates https://github.com/vmware/container-service-extension
+# pip3 install --ignore-installed container-service-extension/ >> /root/pre-install.log
+yum -y remote git >> /root/pre-install.log

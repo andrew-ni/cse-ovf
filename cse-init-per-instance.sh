44b7b881e18e851b4a4c2020ef95740af305f86a
Andrew Ni <niandrew7@gmail.com>
2021-11-18 20:06:41 -0800

    update
    Signed-off-by: Andrew Ni <niandrew7@gmail.com>

--- cse-init-per-instance.sh
+++ cse-init-per-instance.sh
@@ -1,25 +1,19 @@
 #!/bin/bash
-# yum update -y
-# yum -y install git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python
-# pip3 install --upgrade pip
-# git clone -b cse_3_1_updates https://github.com/vmware/container-service-extension
-# pip3 install --ignore-installed container-service-extension/
 
 echo `date` >> /root/cse-init-per-instance.log
-# rpm -Uvvh /var/cache/tdnf/photon/rpms/x86_64/*.rpm >> /root/cse-init-per-instance.log
-# rpm -Uvvh /var/cache/tdnf/photon-updates/rpms/noarch/*.rpm >> /root/cse-init-per-instance.log
-# rpm -Uvvh /var/cache/tdnf/photon-updates/rpms/x86_64/*.rpm >> /root/cse-init-per-instance.log
+# order matters here: gcc needs mpc installed first
+rpm -Uvvh /var/cache/tdnf/photon-updates/rpms/x86_64/*.rpm >> /root/cse-init-per-instance.log
+rpm -Uvvh /var/cache/tdnf/photon/rpms/x86_64/*.rpm >> /root/cse-init-per-instance.log
+rpm -Uvvh /var/cache/tdnf/photon-updates/rpms/noarch/*.rpm >> /root/cse-init-per-instance.log
 
-# unzip container-service-extension.zip
-# pip3 install --ignore-installed container-service-extension/
-# pip3 install --ignore-installed container_service_extension-3.1.2.dev10-py3-none-any.whl >>  /root/cse-init-per-instance.log
+pip3 install --ignore-installed container_service_extension-3.1.2.dev10-py3-none-any.whl >> /root/cse-init-per-instance.log
 
 vmtoolsd --cmd "info-get guestinfo.ovfenv" > /root/ovfenv
 perl -ne 'print $1,"\n" if (m/cse\.configUrl.*oe:value="(.*?)"/)' /root/ovfenv > /root/configUrl
 wget $(cat /root/configUrl) -O /root/cse-config.yaml
 chmod 600 /root/cse-config.yaml
-# cse install -c /root/cse-config.yaml -s -t > /root/cse-install-output.log
-# cse upgrade -c /root/cse-config.yaml -s -t > /root/cse-upgrade-output.log
-# systemctl enable cse
-# systemctl start cse
+cse install -c /root/cse-config.yaml -s -t > /root/cse-install-output.log
+cse upgrade -c /root/cse-config.yaml -s -t > /root/cse-upgrade-output.log
+systemctl enable cse
+systemctl start cse
 echo `date` >> /root/cse-init-per-instance.log
--- post-install.sh
+++ post-install.sh
@@ -1,11 +1,2 @@
 chmod u+x /var/lib/cloud/scripts/per-instance/cse-init-per-instance.sh
-
-# yum update -y >> /root/post-install.log
-# yum -y install git >> /root/post-install.log
-# yum -y install --downloadonly git python3-pip gcc openssl-devel bzip2-devel libffi-devel binutils linux-api-headers libxml2-python >> /root/post-install.log
-# pip3 install --upgrade pip >> /root/post-install.log
-# git clone -b cse_3_1_updates https://github.com/vmware/container-service-extension
-# pip3 install --ignore-installed container-service-extension/ >> /root/post-install.log
-# yum -y remote git >> /root/post-install.log
-
 echo `date` >> /root/post-install.log
